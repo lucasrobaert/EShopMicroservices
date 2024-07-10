a32f3739c65aa4d220b43bde0015864c92ba20b8
Lucas Robaert <lucasrobaert@hotmail.com>
2024-07-10 09:01:34 -0300

    create product

---
+++ src/Services/Catalog/Catalog.Api/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,31 @@
+namespace Catalog.Api.Products.CreateProduct;
+
+public record CreateProductCommand(
+    string Name,
+    List<string> Category,
+    string Description,
+    string ImageFile,
+    decimal Price) : ICommand<CreateProductResult>;
+
+public record CreateProductResult(Guid Id);
+
+internal class CreateProductCommandHandler(IDocumentSession session) : ICommandHandler<CreateProductCommand, CreateProductResult>
+{
+    public async Task<CreateProductResult> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+    {
+        var product = new Product
+        {
+            Name = request.Name,
+            Category = request.Category,
+            Description = request.Description,
+            ImageFile = request.ImageFile,
+            Price = request.Price
+        };
+        
+        session.Store(product);
+        
+        await session.SaveChangesAsync(cancellationToken);
+
+        return new CreateProductResult(product.Id);
+    }
+}
